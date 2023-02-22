ec1cac76b84d9f18cf8fc4f3b82d5b96a646743f
Guilherme Fontes <gui.fontes.amorim@gmail.com>
2023-02-21 22:56:18 -0400

    refactor(clean-react): move to its own file

--- apps/clean-react/jest.config.js
+++ apps/clean-react/jest.config.js
@@ -1,7 +1,12 @@
+const { pathsToModuleNameMapper } = require('ts-jest')
+const { compilerOptions } = require('./tsconfig')
+
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
